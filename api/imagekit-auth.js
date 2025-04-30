f669b159a3ffa0be08dc9658110c0572f92d6884
djonly <djonlykansil@gmail.com>
2025-04-30 23:49:26 +0800

    Update imagekit-auth.js

--- api/imagekit-auth.js
+++ api/imagekit-auth.js
@@ -1,10 +1,13 @@
 import { ImageKit } from "imagekit";
 
 export default function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+
     const imagekit = new ImageKit({
         publicKey: "public_cai3Ahsub6oj/v9ZKtEPJAoD+kw=",
-        privateKey: "private_cksQeyrCwKqskK5nxo548Gd/d9M=",
-        urlEndpoint: "https://ik.imagekit.io/your_imagekit_id"
+        privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+        urlEndpoint: "https://ik.imagekit.io/taloarane"
     });
 
     const result = imagekit.getAuthenticationParameters();
