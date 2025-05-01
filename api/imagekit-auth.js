a7cf659287ccce3e4b5affdcab21743e9097462d
djonly-kansil <djonlykansil@gmail.com>
2025-05-01 11:50:53 +0800

    update imagekit-auth.js

--- api/imagekit-auth.js
+++ api/imagekit-auth.js
@@ -1,26 +1,5 @@
-const ImageKit = require("imagekit");
-
-export default function handler(req, res) {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-	
-	if (req.method === "OPTIONS") {
-		res.status(200).end();
-		return;
-	}
-	
-	if (req.method !== "GET") {
-		res.status(405).json({ error: "Method not allowed" });
-		return;
-	}
-	
-	const imagekit = new ImageKit({
-		publicKey: "public_cai3Ahsub6oj/v9ZKtEPJAoD+kw=",
-		privateKey: "private_cksQeyrCwKqskK5nxo548Gd/d9M=",
-		urlEndpoint: "https://ik.imagekit.io/taloarane"
-	});
-	
-	const result = imagekit.getAuthenticationParameters();
-	res.status(200).json(result);
-}
+const imagekit = new ImageKit({
+	publicKey: "public_cai3Ahsub6oj/v9ZKtEPJAoD+kw=",
+	privateKey: "private_cksQeyrCwKqskK5nxo548Gd/d9M=",
+	urlEndpoint: "https://ik.imagekit.io/taloarane"
+});
