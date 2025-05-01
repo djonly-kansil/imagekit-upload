bb0aa4663a513a6d0f107c06e819b7ad746a1921
djonly-kansil <djonlykansil@gmail.com>
2025-05-01 11:40:28 +0800

    update imagekit-auth.js

--- api/imagekit-auth.js
+++ api/imagekit-auth.js
@@ -17,7 +17,7 @@ export default function handler(req, res) {
 	
 	const imagekit = new ImageKit({
 		publicKey: "public_cai3Ahsub6oj/v9ZKtEPJAoD+kw=",
-		privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+		privateKey: "private_cksQeyrCwKqskK5nxo548Gd/d9M=",
 		urlEndpoint: "https://ik.imagekit.io/taloarane"
 	});
 	
