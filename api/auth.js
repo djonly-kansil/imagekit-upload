b7d9c3a7d3359b3bd7eb2d144cdb074721871284
djonly-kansil <djonlykansil@gmail.com>
2025-05-01 13:33:03 +0800

    update auth.js

--- api/auth.js
+++ api/auth.js
@@ -15,19 +15,26 @@ module.exports = function handler(req, res) {
 		return;
 	}
 	
-	console.log("USING HARDCODED CREDENTIALS");
+console.log("USING HARDCODED CREDENTIALS");
+
+const publicKey = "public_cai3Ahsub6oj/v9ZKtEPJAoD+kw=";
+const privateKey = "private_cksQeyrCwKqskK5nxo548Gd/d9M=";
+const urlEndpoint = "https://ik.imagekit.io/taloarane";
+
+console.log("PUBLIC KEY:", publicKey);
+console.log("PRIVATE KEY:", privateKey ? "OK" : "MISSING");
+console.log("URL ENDPOINT:", urlEndpoint);
+
+try {
+	const imagekit = new ImageKit({
+		publicKey,
+		privateKey,
+		urlEndpoint
+	});
 	
-	try {
-		const imagekit = new ImageKit({
-			publicKey: "public_cai3Ahsub6oj/v9ZKtEPJAoD+kw=",
-			privateKey: "private_cksQeyrCwKqskK5nxo548Gd/d9M=",
-			urlEndpoint: "https://ik.imagekit.io/taloarane"
-		});
-		
-		const result = imagekit.getAuthenticationParameters();
-		res.status(200).json(result);
-	} catch (err) {
-		console.error("ImageKit Auth Error:", err);
-		res.status(500).json({ error: "Internal Server Error" });
-	}
-};
+	const result = imagekit.getAuthenticationParameters();
+	res.status(200).json(result);
+} catch (err) {
+	console.error("ImageKit Auth Error:", err);
+	res.status(500).json({ error: "Internal Server Error" });
+}
