0e7f27ffcf2e36b627674811a756964abc333437
djonly-kansil <djonlykansil@gmail.com>
2025-05-04 23:18:09 +0800

    update get-signature.js

--- pages/api/get-signature.js
+++ pages/api/get-signature.js
@@ -1,6 +1,6 @@
 import crypto from "crypto";
 import { initializeApp, cert } from "firebase-admin/app";
-import { getAuth } from "../../lib/firebase-admin";
+import { getAuth } from "../../lib/firebaseAdmin";
 
 const app = !global._firebaseApp ? initializeApp({
 	credential: cert({
